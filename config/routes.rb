db65a62a682424881e5ecd1f6c101ce64ffa8e43
srshifke <steven.shifke@gmail.com>
2015-05-18 23:36:37 -0400

    Added Recipes Controller and defined index action and index view. Added gravatar functionallity/method to the index view/application_helper. Added custom CSS to handle classes associated with the index view.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 	root 'pages#home';
 	
   get '/home' => 'pages#home'
+
+  #get '/recipes' => 'recipes#index'
+  #get '/recipes/new' => 'recipes#new', as: 'new_recipe'
+  #post '/recipes' => 'recipes#create'
+  #get '/recipes/:id/edit' => 'recipes#edit', as: 'edit_recipe'
+  #patch '/recipes/:id' => 'recipes#update'
+  #put '/recipes/:id' => 'recipes#update'
+  #get '/recipes/:id' => 'recipes#show', as: 'recipe'
+  #delete '/recipes/:id' => 'recipes#destroy'
+
+  #the following creates all the above by default
+  resources :recipes
 end
