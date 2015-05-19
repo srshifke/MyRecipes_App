977aeed26c4d48d64103e95ab8ad7a7b2046ee96
srshifke <steven.shifke@gmail.com>
2015-05-18 22:26:33 -0400

    Created/Added migration to add chef_id foreign key column. Added associations between Recipes and Chefs. Added new validation/tests to Recipes model/test-suite to assure it is properly associated with Chefs.

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,12 +3,18 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
 	def setup
-		@recipe = Recipe.new(name: "Chicken Parmesan", summary: "This is the best chicken parmesan ever!", 
+		@chef = Chef.create(chefname: "Bob", email: "bob@example.com")
+		@recipe = @chef.recipes.build(name: "Chicken Parmesan", summary: "This is the best chicken parmesan ever!", 
 							description: "1. Heat Oil 2. Add Onion 3. Add Tomato Sauce 4. Add Chicken 5. Cook for 20 minutes")
 	end
 
 	test "recipe should be valid" do
 		assert @recipe.valid?
+	end
+
+	test "chef_id should be present" do
+		@recipe.chef_id = nil
+		assert_not @recipe.valid?
 	end
 
 	test "name should be present" do
