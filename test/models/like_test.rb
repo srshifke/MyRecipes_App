d9a3cd6f7b320ff7fc5c84923006564356b58faf
srshifke <steven.shifke@gmail.com>
2015-05-25 18:48:33 -0400

    Added the Styles, Ingredients, Recipe_Styles, and Recipe_Ingredients migrations/tables and associated views. Updated views to allow has_many, through: relationships between recipes and ingredients and recipes and styles.

---
+++ test/models/like_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+
+	def setup
+
+	end
+
+	test 'user can only vote for a recipe once: up-vote' do
+
+	end
+
+	test 'user can only vote for a recipe once: down-vote' do
+
+	end
+
+	test 'creator cannot up-vote own recipe' do
+		
+	end
+
+	test 'creator cannot down-vote own recipe' do
+		
+	end
+
+end
